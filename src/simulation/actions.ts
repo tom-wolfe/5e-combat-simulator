b5493aa2671a65d8487869262844dd0ea770d998
Tom Wolfe <trwolfe13@gmail.com>
2018-10-08 07:16:21 +0100

    Update todo note.

--- src/simulation/actions.ts
+++ src/simulation/actions.ts
@@ -1,6 +1,6 @@
 import { Action, Creature } from '@sim/models';
 
 export function possibleActions(creature: Creature): Action[] {
-    // TODO: Filter by resources (usage and spell slots).
+    // TODO: Filter by spell slots.
   return creature.actions.filter(c => c.uses === undefined || c.uses > 0);
 }
