c0589cca1c47f1da47e1ebd8bace0e1103927412
Tom Wolfe <TOM356@bailliegifford.com>
2018-10-08 10:40:48 +0100

    Refactor.

--- src/simulation/targets.ts
+++ src/simulation/targets.ts
@@ -31,5 +31,5 @@ export function canKill(actions: Action[], targets: Creature[]): Creature[] {
 
 export function mostDangerous(targets: Creature[]): Creature[] {
   // TODO: Factor resistances and stuff.
-  return [max(targets, t => highestAverage(t.actions).damage).object];
+  return [max(targets, t => highestAverage(t.actions).damage).object].filter(t => t);
 }
